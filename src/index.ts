4e8c178d834dc711ff42a6645c581d228aa12d38
Alex N. Jose <alexnj@gmail.com>
2023-02-21 12:36:58 -0800

    Rewrite atticandbutton.us through the worker.

--- src/index.ts
+++ src/index.ts
@@ -22,12 +22,23 @@ export interface Env {
 	// MY_SERVICE: Fetcher;
 }
 
+import packageJson from '../package.json';
+const identificationString = `${packageJson.name}/${packageJson.version}`;
+
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
-	},
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    // Rewrite only read operations.
+    if (request.method !== 'GET') return new Response(null, {status: 405});
+
+    const { pathname, search } = new URL(request.url);
+    const rewrittenUrl = new URL(pathname + search, 'https://www.atticandbutton.us');
+    return fetch(rewrittenUrl, {
+      headers: {
+        'content-type': 'text/html;charset=utf-8',
+        // Modify this request just enough to make rewrites work and
+        // identify ourselves if Shopify wants to filter traffic.
+        'user-agent': `Mozilla/5.0 (Macintosh; Intel Mac OS X 13_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 ${identificationString}`,
+      },
+    });
+  }
 };
